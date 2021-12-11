f02cf77e805d729ec023e04fe1e430c5eeed91a1
Francisco Casiano <34542704+panchi64@users.noreply.github.com>
2021-12-11 15:38:56 -0400

    Updated start.sh for correct jar pathing

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-
-screen -d -m -S mcServer java -Xms2G -Xmx8G -jar paper-1.18-57.jar --nogui
+# Change the [path/to/jar] to the path where the minecraft server jar is located
+screen -d -m -S mcServer java -Xms2G -Xmx8G -jar [/path/to/jar/]paper-1.18-57.jar --nogui
